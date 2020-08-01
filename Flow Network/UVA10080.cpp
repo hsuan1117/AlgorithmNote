0ef6f94cf5d18abf7de8525f560f4490ec929875
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-08-01 16:57:56 +0800

    Add

--- Flow Network/UVA10080.cpp
+++ Flow Network/UVA10080.cpp
@@ -4,7 +4,8 @@
 #define int long long
 #define INF 0x3f3f3f
 using namespace std;
-const int N = 220;
+const int N = 1040;
+const int PREFIX = 520;
 /*=====================================*/
 struct Point {
   double x,y;
@@ -17,9 +18,9 @@ double GetDistance(Point a, Point b){
   return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 }
 /*=====================================*/
-
+int ratN,holeN,timeout,speed;
 int used[N],matched[N];
-int V[N]={0};
+vector<int> V[N];
 //Point rats[N]={},holes[N]={};
 
 bool dfs(int v){
@@ -35,14 +36,14 @@ bool dfs(int v){
 }
 // x -> y :: Max c
 void addEdge(int i,int j){
-  V[i] = j;
-  V[j] = i;
+  V[i].pb(j+PREFIX);
+  V[j+PREFIX].pb(i);
 }
-int solve(int s,int e){
+int solve(){
   //Konig
   memset(matched,-1,sizeof(matched));
   int ans = 0;
-  for(int i=1;i<=n;i++){
+  for(int i=1;i<=ratN;i++){
     memset(used,0,sizeof(used));
     if(dfs(i))ans++;
   }
@@ -52,16 +53,17 @@ signed main(){
   ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
   //freopen("1.in", "r", stdin);
   //freopen("1.out", "w", stdout);
-  int ratN,holeN,timeout,speed;
-
   while(cin >> ratN >> holeN >> timeout >> speed){
+    for(int i=1;i<=N;i++)V[i].clear();
     for(int i=1;i<=ratN;i++){
-      double xx,yy;cin >> xx >> yy;
-      rats[i] = Point(xx,yy);
+      Point ppt ;
+      double xx,yy;cin >> ppt.x >> ppt.y;
+      rats[i] = ppt;
     }
     for(int i=1;i<=holeN;i++){
-      double xx,yy;cin >> xx >> yy;
-      holes[i] = Point(xx,yy);
+      Point ppt ;
+      double xx,yy;cin >> ppt.x >> ppt.y;
+      holes[i] = ppt;
     }
     for(int i=1;i<=ratN;i++){
       Point rat = rats[i];
@@ -72,6 +74,7 @@ signed main(){
         }
       }
     }
+    cout << ratN-solve() << endl;
   }
   return 0;
 }
